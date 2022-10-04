25fce08c6934674679ddb232e3a10cfb46fd34a8
Junior Rodriguez <junior.david.rodriguez@gmail.com>
2022-10-04 12:41:30 -0400

    creacion de vistas html basicas para la app

--- gest_lab/views.py
+++ gest_lab/views.py
@@ -3,19 +3,26 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse('inicio')
+    #return HttpResponse('inicio')
+    return render(request, 'gest_lab/inicio.html')
 
 def solicitar(request):
-    return HttpResponse('solicitar')
-
+    #return HttpResponse('solicitar')
+    return render(request, 'gest_lab/solicitar.html')
+    
 def procesar(request):
-    return HttpResponse('procesar')
-
+    #return HttpResponse('procesar')
+    return render(request, 'gest_lab/procesar.html')
+    
 def validar(request):
-    return HttpResponse('validar')
-
+    #return HttpResponse('validar')
+    return render(request, 'gest_lab/validar.html')
+    
 def entregar(request):
-    return HttpResponse('entregar')
-
+    #return HttpResponse('entregar')
+    return render(request, 'gest_lab/entregar.html')
+    
 def configuracion(request):
-    return HttpResponse('configuracion')
+    #return HttpResponse('configuracion')
+    return render(request, 'gest_lab/configuracion.html')
+    
