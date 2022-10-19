60ed3bb6cefdc78e29c1311ef1b8e081d0edead3
Junior Rodriguez <junior.david.rodriguez@gmail.com>
2022-10-18 22:41:23 -0400

    se agregan los filtros a procesar, validar y entregar

--- gest_lab/views.py
+++ gest_lab/views.py
@@ -38,46 +38,55 @@ def solicitar(request):
 def procesar(request):
     #return HttpResponse('procesar')
     l_analisis = analisis.objects.all()
-    l_examenes = {}
+    l_examenes = examen.objects.all()
+    null = None
+
     if request.method == 'GET':
         ced = request.GET.get('cedula') if request.GET.get('cedula') != None else ''
         n_ord = request.GET.get('n_orden') if request.GET.get('n_orden') != None else ''
         t_ex = request.GET.get('t_examen') if request.GET.get('t_examen') != None else 0
-        if t_ex == '':
-            l_examenes = examen.objects.values('n_orden','id_cliente__cedula','id_analisis__nombre').filter(id_cliente__cedula__startswith=ced,n_orden__endswith=n_ord).annotate(dcount=Count('n_orden')).order_by()
+        if t_ex == 0:
+            l_examenes = examen.objects.values('n_orden','id_cliente__cedula','id_analisis__nombre').filter(id_cliente__cedula__startswith=ced,n_orden__endswith=n_ord,f_proceso=None,f_validado=None,f_entrega=None).annotate(dcount=Count('n_orden')).order_by()
         else:
-            l_examenes = examen.objects.values('n_orden','id_cliente__cedula','id_analisis__nombre').filter(id_cliente__cedula__startswith=ced,n_orden__endswith=n_ord,id_analisis=t_ex).annotate(dcount=Count('n_orden')).order_by()
-        # print(l_examenes.query)
-    return render(request, 'gest_lab/procesar.html',{'dir_ex':'/procesar', 'analisis':l_analisis, 'l_examenes':l_examenes, 'query':l_examenes.query})
+            l_examenes = examen.objects.values('n_orden','id_cliente__cedula','id_analisis__nombre').filter(id_cliente__cedula__startswith=ced,n_orden__endswith=n_ord,id_analisis=t_ex,f_proceso=None,f_validado=None,f_entrega=None).annotate(dcount=Count('n_orden')).order_by()
+    
+    else:
+        l_examenes = examen.objects.all()
+    
+    print(l_examenes)
+    return render(request, 'gest_lab/procesar.html',{'dir_ex':'/procesar', 'analisis':l_analisis, 'examenes':l_examenes})
     
 def validar(request):
     #return HttpResponse('validar')
-    cli = {}
-    if request.method == 'POST':
-        cli = cliente.objects.filter(cedula=request.POST['text_cedula']).values()
-        if len(cli) == 0:
-            c = cliente(cedula=request.POST['text_cedula'],
-                        nombre=request.POST['text_nombre_cliente'],
-                        apellido=request.POST['text_apellido_cliente'],
-                        f_nac=request.POST['text_fecha_nacimiento'],
-                        direccion=request.POST['text_direccion_cliente'],
-                        telefono=request.POST['text_telefono_cliente'],
-                        id_sexo= sexo.objects.get(id=request.POST['radio_sexo'])
-                        )
-            c.save()
+    l_analisis = analisis.objects.all()
+    l_examenes = examen.objects.all()
+    if request.method == 'GET':
+        ced = request.GET.get('cedula') if request.GET.get('cedula') != None else ''
+        n_ord = request.GET.get('n_orden') if request.GET.get('n_orden') != None else ''
+        t_ex = request.GET.get('t_examen') if request.GET.get('t_examen') != None else 0
+        if t_ex == 0:
+            l_examenes = examen.objects.values('n_orden','id_cliente__cedula','id_analisis__nombre').filter(id_cliente__cedula__startswith=ced,n_orden__endswith=n_ord,f_validado=None,f_entrega=None).exclude(f_proceso=None).annotate(dcount=Count('n_orden')).order_by()
+        else:
+            l_examenes = examen.objects.values('n_orden','id_cliente__cedula','id_analisis__nombre').filter(id_cliente__cedula__startswith=ced,n_orden__endswith=n_ord,id_analisis=t_ex,f_validado=None,f_entrega=None).exclude(f_proceso=None).annotate(dcount=Count('n_orden')).order_by()
+        # print(l_examenes.query)
+    return render(request, 'gest_lab/validar.html',{'dir_ex':'/procesar', 'analisis':l_analisis, 'l_examenes':l_examenes})
 
-    anio = datetime.datetime.now()
-    edad = 0
-    for x in cli:
-        edad = anio.year - x['f_nac'].year
-    anio2 = anio.strftime("%Y-%m-%d %H:%M")
-
-
-    return render(request, 'gest_lab/validar.html',{'dir_ex':'/validar','cli':cli, 'edad':edad, 'anio':anio2})
     
 def entregar(request):
     #return HttpResponse('entregar')
-    return render(request, 'gest_lab/entregar.html',{'dir_ex':'/entregar'})
+    l_analisis = analisis.objects.all()
+    l_examenes = examen.objects.all()
+    if request.method == 'GET':
+        ced = request.GET.get('cedula') if request.GET.get('cedula') != None else ''
+        n_ord = request.GET.get('n_orden') if request.GET.get('n_orden') != None else ''
+        t_ex = request.GET.get('t_examen') if request.GET.get('t_examen') != None else 0
+        if t_ex == 0:
+            l_examenes = examen.objects.values('n_orden','id_cliente__cedula','id_analisis__nombre').filter(id_cliente__cedula__startswith=ced,n_orden__endswith=n_ord,f_entrega=None).exclude(f_validado=None).exclude(f_proceso=None).annotate(dcount=Count('n_orden')).order_by()
+        else:
+            l_examenes = examen.objects.values('n_orden','id_cliente__cedula','id_analisis__nombre').filter(id_cliente__cedula__startswith=ced,n_orden__endswith=n_ord,id_analisis=t_ex,f_entrega=None).exclude(f_validado=None).exclude(f_proceso=None).annotate(dcount=Count('n_orden')).order_by()
+        # print(l_examenes.query)
+    return render(request, 'gest_lab/entregar.html',{'dir_ex':'/procesar', 'analisis':l_analisis, 'l_examenes':l_examenes, 'query':l_examenes.query})
+
     
 def configuracion(request):
     #return HttpResponse('configuracion')
